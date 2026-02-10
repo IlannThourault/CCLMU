969f6b6d3d44bdf9bee25dd3d52671d3e7747206
NathanLBZ <nathanmorin.726@gmail.com>
2026-02-10 08:20:27 +0100

    pour ilann

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from './components/helloWorld/hello.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HelloComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
   protected readonly title = signal('CCLMU');
 }
