c1361e1076d4f7627e6aaad7ad4c1754fa898eab
Ilann Thourault <ilann.thourault@gmail.com>
2026-04-02 10:39:58 +0200

    rm menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,11 @@ import { SliderComponent } from './components/slider/slider';
 import { ChampsRecherche } from './components/champs-recherche/champs-recherche';
 import { BarreRecherche } from './components/barre-recherche/barre-recherche';
 import { ProjectListComponent } from './components/project-list/project-list.component';
-import {Menu} from "./components/menu/menu";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MapComponent, SliderComponent, ChampsRecherche, BarreRecherche, ProjectListComponent, Menu],
+  imports: [RouterOutlet, MapComponent, SliderComponent, ChampsRecherche, BarreRecherche, ProjectListComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
